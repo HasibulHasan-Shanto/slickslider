66dfe0929f6ee226b735db69025d687a26694345
shanto <shantohasibulhasan379@gmail.com>
2024-09-19 08:54:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('#sliderPart').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '  <i class="fa-solid fa-angle-left left"></i>',
+    nextArrow: '  <i class="fa-solid fa-angle-right right"></i>',
+    dots: true
+  });
+  
+
+
